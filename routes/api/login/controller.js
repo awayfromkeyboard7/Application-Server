5911af52c1778842f8bcd6eff25a60db21194fdf
Son0-0 <jsmith032211@gmail.com>
2022-07-10 17:44:39 +0900

    dinner nyam

--- routes/api/login/controller.js
+++ routes/api/login/controller.js
@@ -4,4 +4,5 @@ exports.githubLogin = (req, res) => {
   console.log("HERE");
   const url = `https://github.com/login/oauth/authorize?client_id=${client_id}`
   res.json({url: url}); // redirect issue
+  // res.redirect(url);
 }
