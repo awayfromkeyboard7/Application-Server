ace4856e054ac96cec2b5c4646569cd8a9b6669c
Son0-0 <jsmith032211@gmail.com>
2022-07-15 17:13:13 +0900

    invite member temp

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const User = require("./models/user");
 const GameLog = require("./models/gamelog");
 const GameRoom = require("./models/gameroom")
+const uuid = require('uuid');
 
 const SocketIO = require("socket.io");
 const server = http.createServer(app);
@@ -28,9 +29,29 @@ app.use(express.json());
 app.use("/", require("./routes/"));
 
 let teamRoom = {}
+// user들의 socket Id
+let usersSocketId = {}
+
+// {
+//   'Son0-0' : {
+//     room: dsfklsdjflksjflksdjflksdjf,
+//     user: [Son0-0, park-hg, ...]
+//   }
+// }
+
+
 
 io.on("connection", (socket) => {
   console.log(`user connected: ${socket.id}`);
+
+  socket.on('setGitId', gitId => {
+    // socket["gitId"] = gitId;
+    // if (gitId) {
+      // socket.on('setGitId')
+    usersSocketId[gitId] = socket.id;
+    console.log('usersSocketId>>>', usersSocketId)
+    // }
+  })
 
   SocketRoutes.waitGame(socket, SocketRoutes.event.waitGame);
   SocketRoutes.startGame(socket, SocketRoutes.event.startGame);
@@ -44,15 +65,39 @@ io.on("connection", (socket) => {
 
   socket.on('createTeam', (userInfo) => {
     console.log('createTeam........');
-    socket.join(socket.id);
-    teamRoom[socket.id] = [userInfo]
-    socket.emit('enterNewUserToTeam', teamRoom[socket.id]);
+    const teamRoomId = uuid.v4();
+    socket.join(teamRoomId);
+    teamRoom[teamRoomId] = [userInfo];
+    console.log('teamRoom>>>>', teamRoomId)
+    // socket["nickname"] = teamRoomId;
+    socket.to(socket.id).emit('setTeamRoomId', teamRoomId);
+    console.log(socket["teamRoomId"])
+    // 퍼플
+    socket.emit('enterNewUserToTeam', teamRoom[teamRoomId]);
     }
   )
 
-  socket.on('inviteMember', (from, to) => {
-    socket.emit('inviteMember', to);
-    socket.emit('enterNewUserToTeam', teamRoom[socket.id]);
+  socket.on("setTeamRoomId", (teamRoomId) => {
+    console.log("setTeamRoomIdsetTeamRoomIdsetTeamRoomId")
+    socket["teamRoomId"] = teamRoomId;
+  })
+
+  socket.on('inviteMember', (roomId, friendGitId) => {
+    console.log('inviteMember roomId>>>>>', roomId, friendGitId);
+    socket.to(usersSocketId[friendGitId]).emit('comeon', roomId);
+    // to.join(from.teamRoomId)
+    // socket.emit('inviteMember', to);
+    // socket.emit('enterNewUserToTeam', teamRoom[from.id]);
+    }
+  )
+
+  socket.on('acceptInvite', (roomId, userInfo) => {
+    // to.join(from.teamRoomId)
+    // socket.emit('inviteMember', to);
+    // socket.emit('enterNewUserToTeam', teamRoom[from.id]);
+    teamRoom[roomId].push(userInfo)
+    socket.join(roomId);
+    socket.emit('enterNewUserToTeam', teamRoom[roomId]);
     }
   )
 });
