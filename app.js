4badbce9aa37eb4c04b33fa7712a94485bd307df
dd0114 <ehgus0114@gmail.com>
2022-07-20 22:16:21 +0900

    5초뒤 실행 추가

--- app.js
+++ app.js
@@ -210,6 +210,7 @@ io.on("connection", (socket) => {
       // 새로고침하면 이미 내가 대기리스트에 있는 상태.
       if (!(waitingList.includes(roomId))) {
         const gameLogId = await gamelog.createTeamLog(getPlayers(teamRoom[waitingList[0]]), getPlayers(teamRoom[roomId]), teamRoom[waitingList[0]].id, teamRoom[roomId].id);
+        User.addgamelog(await GameLog.getLog(gameLogId));
         let timeLimit = new Date();
         timeLimit.setSeconds(timeLimit.getSeconds() + 5);
         const firstTeamId = teamRoom[waitingList[0]].id;
@@ -217,6 +218,10 @@ io.on("connection", (socket) => {
 
         const interval = setInterval(() => {
           socket.nsp.to([firstTeamId,secondTeamId]).emit("timeLimitCode", timeLimit - new Date());
+          if(timeLimit < new Date()) {
+            clearInterval(interval);
+          }
+
         }, 1000);
 
         setTimeout(() => {
