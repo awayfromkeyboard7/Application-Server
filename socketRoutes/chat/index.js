0a31c05a2cec4cd1afaa3a6e30a305c8c9b416ff
Son0-0 <jsmith032211@gmail.com>
2022-07-22 22:34:12 +0900

    feat: unread Message

--- socketRoutes/chat/index.js
+++ socketRoutes/chat/index.js
@@ -1,3 +1,5 @@
 exports.event = require('./event');
 exports.getChatMessage = require('./getChatMessage');
 exports.sendChatMessage = require('./sendChatMessage');
+exports.getUnreadMessage = require('./getUnreadMessage');
+exports.resetUnreadCount = require('./resetUnreadCount');
