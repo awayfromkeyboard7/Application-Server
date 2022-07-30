c408233710c99763e045dba0f12b1da011b4e21b
EilLagerTodd <lookea84@gmail.com>
2022-07-31 03:03:51 +0900

    add: userRankLog/userLangData

--- routes/api/gamelog/controller.js
+++ routes/api/gamelog/controller.js
@@ -14,7 +14,7 @@ exports.updateGamelogTeam = async (req, res) => {
       await GameLog.updateLogTeam(req.body);
       if (await GameLog.isFinishTeam(req.body)) {
         const gameLog = await GameLog.getLog(req.body["gameId"])
-        Interval.deleteInterval([gameLog["roomIdA"],gameLog["roomIdB"]],'team');
+        Interval.deleteInterval([gameLog["roomIdA"], gameLog["roomIdB"]], 'team');
         Ranking.updateRanking(await User.totalRankUpdate());
       }
       res.status(200).json({
@@ -26,7 +26,7 @@ exports.updateGamelogTeam = async (req, res) => {
         message: 'Invalid JWT Token'
       });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(409).json({
       success: false,
       message: err.message
@@ -40,9 +40,9 @@ exports.updateGamelog = async (req, res) => {
     if (payload.gitId === req.body.gitId) {
       await GameLog.updateLog(req.body);
       if (await GameLog.isFinish(req.body)) {
-  
+
         const gameLog = await GameLog.getLog(req.body.gameId);
-        Interval.deleteInterval(gameLog["roomId"],'solo')
+        Interval.deleteInterval(gameLog["roomId"], 'solo')
         await Ranking.updateRanking(await User.totalRankUpdate());
       }
       res.status(200).json({
@@ -54,7 +54,7 @@ exports.updateGamelog = async (req, res) => {
         message: 'Invalid JWT Token'
       });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(409).json({
       success: false,
       message: err.message
@@ -67,16 +67,16 @@ exports.createGamelog = async (req, res) => {
     const payload = await Auth.verify(req.cookies['jwt']);
     if (payload !== false) {
       const info = {
-        problemId : await Problem.random(),
+        problemId: await Problem.random(),
         userHistory: req.body.players,
         totalUsers: req.body.totalUsers,
-        roomId : req.body.roomId
+        roomId: req.body.roomId
       }
       const gameLog = await GameLog.createLog(info);
       User.addGameLog(gameLog);
       info.userHistory.forEach(item => console.log(item.gitId))
       res.status(200).json({
-        gameLogId : gameLog._id,
+        gameLogId: gameLog._id,
         success: true
       });
     } else {
@@ -85,7 +85,7 @@ exports.createGamelog = async (req, res) => {
         message: 'Invalid JWT Token'
       });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(409).json({
       success: false,
       message: err.message
@@ -99,7 +99,7 @@ exports.getProblem = async (req, res) => {
     if (payload !== false) {
       logId = req.body['gameLogId']
       let info = await GameLog.getLog(logId);
-  
+
       const problemId = mongoose.Types.ObjectId(req.body.mode === 'team' ? '62cea4c0de41eb81f44ed976' : '62e0f67f8f1ac997694d4e86');
       const problems = await Problem.getProblem(problemId);
       info.problemId = problems
@@ -113,7 +113,7 @@ exports.getProblem = async (req, res) => {
         message: 'Invalid JWT Token'
       });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(409).json({
       success: false,
       message: err.message
@@ -138,10 +138,81 @@ exports.getGamelog = async (req, res) => {
         message: 'Invalid JWT Token'
       });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(409).json({
       success: false,
       message: err.message
     });
   }
 };
+
+
+
+exports.userRankLog = async (req, res) => {
+  try {
+    const userGitId = req.body.userId;
+    const UserInfo = await User.getUserInfo(req.body.userId);
+    const LangInfo = UserInfo.language
+    const langLength = Object.keys(LangInfo).length
+    const langKey = Object.keys(LangInfo)
+    const langValue = Object.values(LangInfo)
+    const gameLogId = UserInfo.gameLogHistory.reverse()
+    const gameLogIdLength = Object.keys(gameLogId).length
+    const LangArr = []
+    const RankLogArr = []
+
+    for (let i = 0; i < langLength; i++) {
+
+    }
+
+    for (let i = 0; i < langLength; i++) {
+      LangArr.push({ name: langKey[i], value: langValue[i] })
+    }
+
+
+    function leftPad(value) {
+      if (value >= 10) {
+        return value;
+      }
+      return `0${value}`;
+    }
+
+    function toStringByFormatting(source, delimiter = '.') {
+      const month = leftPad(source.getMonth() + 1);
+      const day = leftPad(source.getDate());
+      return [month, day].join(delimiter);
+    }
+
+    for (let i = 0; i < gameLogIdLength; i++) {
+      let gameLog = await GameLog.getLog(gameLogId[i]);
+      let logLength = Object.keys(gameLog.userHistory).length
+      if (gameLog.gameMode === 'personal') {
+        for (let j = 0; j < logLength; j++) {
+          let userHistoryString = JSON.stringify(gameLog.userHistory[j].userId)
+          if (userHistoryString.includes(userGitId) && gameLog.userHistory[j].ranking !== 0) {
+            const ranking = gameLog.userHistory[j].ranking;
+            const submitAt = gameLog.userHistory[j].submitAt
+            const key = toStringByFormatting(submitAt);
+            if (RankLogArr.length >= 10) {
+              break;
+            } else {
+              RankLogArr.unshift({ date: key, rank: ranking });
+            }
+          }
+        }
+      }
+    }
+
+    res.status(200).json({
+      LangArr,
+      RankLogArr,
+      success: true
+    });
+  } catch (err) {
+    res.status(409).json({
+      success: false,
+      message: err.message
+    });
+  }
+
+}
--- routes/api/gamelog/index.js
+++ routes/api/gamelog/index.js
@@ -7,4 +7,7 @@ router.post('/getProblem', controller.getProblem);
 router.post('/getGameLog', controller.getGamelog);
 router.post('/updateTeam', controller.updateGamelogTeam);
 
+// 유저 개인전 랭킹 (최근 10회) 가져오는
+router.post('/userRankLog', controller.userRankLog)
+
 module.exports = router;
