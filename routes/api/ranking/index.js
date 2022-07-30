117e6ab5dadacc3e224a0a32fbaf5e0bf8328d7e
Son0-0 <jsmith032211@gmail.com>
2022-07-31 01:58:12 +0900

    refactor: remove ranking/paging code

--- routes/api/ranking/index.js
+++ routes/api/ranking/index.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 router.get('/getRanking', controller.getAllRanking);
-router.post('/paging', controller.paging);
 
 module.exports = router;
