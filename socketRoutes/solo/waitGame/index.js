43aaa1167debd37ee2430a74c3c6e821e324061e
park-hg <jogatpark@gmail.com>
2022-08-02 18:02:49 +0900

    fix: remove auto start when room is full

--- socketRoutes/solo/waitGame/index.js
+++ socketRoutes/solo/waitGame/index.js
@@ -23,8 +23,6 @@ module.exports = (socket, event) => {
           let timeLimit = new Date();
           timeLimit.setMinutes(timeLimit.getMinutes() + 3);
           Interval.makeInterval(socket, `room${idx}`, timeLimit, "wait")
-        
-          socket.nsp.to(`room${idx}`).emit('enterNewUser', GameRoom.room[idx].players);
         } 
 
         // 기존에 있던 개인전 대기룸에 들어감
@@ -39,15 +37,8 @@ module.exports = (socket, event) => {
 
           GameRoom.setRoom(unique);
           socket.join(`room${idx}`);
-
-          socket.nsp.to(`room${idx}`).emit('enterNewUser', GameRoom.room[idx].players);
-
-          // 유저가 8명이면 게임 자동 시작
-          if (GameRoom.room[idx].players.length === 8) {
-            socket.emit('getRoomId', `room${idx}`, 'waiting');
-            GameRoom.setStatus(idx, 'playing');
-          }
         }
+        
         else {
           idx = GameRoom.increaseIdx();
           GameRoom.createRoom(userInfo);
@@ -56,10 +47,9 @@ module.exports = (socket, event) => {
           let timeLimit = new Date();
           timeLimit.setMinutes(timeLimit.getMinutes() + 3);
           Interval.makeInterval(socket, `room${idx}`, timeLimit, "wait");
-
-          socket.nsp.to(`room${idx}`).emit('enterNewUser', GameRoom.room[idx].players);
         }
 
+        socket.nsp.to(`room${idx}`).emit('enterNewUser', GameRoom.room[idx].players);
         console.log('solo rooms after', GameRoom.room);
       
       }
