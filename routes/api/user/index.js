2215af865510331cfa301c30e9b13a1e6ce0f47d
park-hg <jogatpark@gmail.com>
2022-08-04 14:28:41 +0900

    refactor: paging -> rank

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -4,7 +4,7 @@ const controller = require("./controller");
 router.get('/login', controller.getGitInfo);
 router.get('/info', controller.getUserInfo);
 router.get('/search', controller.searchUser);
-router.get('/paging', controller.pagingRanking);
+router.get('/rank', controller.pagingRanking);
 router.get('/count', controller.countUser);
 
 module.exports = router;
